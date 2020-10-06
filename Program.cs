35835df9b35f24186b64b38ff0f1fa4d9a5a312d
Luélly Cabral <luellycabral21@gmail.com>
2020-10-06 13:50:09 -0300

    Adiciona a resposta do exercicio

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Somade2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor01 ; 
+            int valor02 ;
+
+            Console.WriteLine("Escreva o V01 : ");
+            valor01 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva o V02 :");
+            valor02 = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            Console.WriteLine($" Sua conta é: { valor01 + valor02 }");
+
+            Console.WriteLine("click ENTER para sair");
+            Console.ReadKey();
+            
+
+
+
+
+        }
+    }
+}
